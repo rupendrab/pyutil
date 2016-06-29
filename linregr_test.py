7b76fdfb0303fd82fd705dfc2954eed83b9cf822
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-06-29 12:47:44 -0400

    Update linregr_test.py

--- linregr_test.py
+++ linregr_test.py
@@ -30,3 +30,22 @@ X_test = [[8], [9], [11], [16], [12]]
 y_test = [[11], [8.5], [15], [18], [11]]
 print('R-squared: %.4f' % model.score(X_test, y_test))
 
+from numpy.linalg import inv
+from numpy import dot, transpose
+
+X = [[1,6,2],[1,8,1],[1,10,0],[1,14,2],[1,18,0]]
+y = [[7], [9], [13], [17.5], [18]]
+coeffs = dot(inv(dot(transpose(X), X)), dot(transpose(X), y))
+print(coeffs)
+
+X = [[6,2],[8,1],[10,0],[14,2],[18,0]]
+y = [[7],[9],[13],[17.5],[18]]
+model = LinearRegression()
+model.fit(X, y)
+X_test = [[8,2],[9,0],[11,2],[16,2],[12,0]]
+y_test = [[11],[8.5],[15],[18],[11]]
+predictions = model.predict(X_test)
+for i, prediction in enumerate(predictions):
+  print('Predicted: %s, Target: %s' % (prediction, y_test[i]))
+print('R squared: %.4f' % model.score(X_test, y_test))
+
