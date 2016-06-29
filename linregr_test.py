091062145cb8efb01515970c34094902025da2e8
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-06-29 13:26:43 -0400

    Update linregr_test.py

--- linregr_test.py
+++ linregr_test.py
@@ -52,3 +52,38 @@ for i, prediction in enumerate(predictions):
   print('Predicted: %s, Target: %s' % (prediction, y_test[i]))
 print('R squared: %.4f' % model.score(X_test, y_test))
 
+## Some List test (adding square of value as another column in numpy array)
+X = np.array([[6],[8],[10],[14],[18]])
+X_q = [np.append(x, x[0] **2) for x in X]
+print(X_q)
+
+## Example of polynomial regression
+%matplotlib notebook
+
+from sklearn.preprocessing import PolynomialFeatures
+X_train = [[6],[8],[10],[14],[18]]
+y_train = [[7],[9],[13],[17.5],[18]]
+X_test = [[6],[8],[11],[16]]
+y_test = [[8],[12],[15],[18]]
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+xx = np.linspace(0, 26, 100)
+yy = regressor.predict(xx.reshape(xx.shape[0], 1))
+plt.plot(xx, yy)
+plt.scatter(X_train, y_train)
+
+quadratic_featurizer = PolynomialFeatures(degree=2)
+X_train_quadratic = quadratic_featurizer.fit_transform(X_train)
+xx_quadratic = quadratic_featurizer.fit_transform(xx.reshape(xx.shape[0], 1))
+
+regressor_quadratic = LinearRegression()
+regressor_quadratic.fit(X_train_quadratic, y_train)
+
+plt.plot(xx, regressor_quadratic.predict(xx_quadratic), c='r', linestyle="--")
+plt.title('Pizza price regressed on diameter')
+plt.xlabel('Diameter in inches')
+plt.ylabel('Price in dollars')
+plt.axis([0,25,0,25])
+plt.grid(True)
+plt.show()
